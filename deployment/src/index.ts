33820a68d3c35f2f90261996f0a4134ba0fc6463
Aradhya <aradhya@my.yorku.ca>
2024-02-14 18:19:10 -0500

    Deployment IV

--- deployment/src/index.ts
+++ deployment/src/index.ts
@@ -3,8 +3,10 @@ import { copyFinalDist, downloadS3Folder } from './aws';
 import { buildProject } from './utils';
 
 const subscriber = createClient();
-
 subscriber.connect().then(() => {main();});
+
+const publisher = createClient();
+subscriber.connect();
 
 async function main() {
     while(1) {
@@ -17,6 +19,7 @@ async function main() {
         await downloadS3Folder(`output/${id}`)
         console.log('downloaded');
         await buildProject(id);
-        await copyFinalDist(id);
+         copyFinalDist(id);
+         publisher.hSet("status", id, "deployed");
     }
 }
