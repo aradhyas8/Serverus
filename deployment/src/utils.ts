ba3df1ec271db13eca35c3c224d674421c58ff1b
Aradhya <aradhya@my.yorku.ca>
2024-02-21 17:43:51 -0500

    Updated build function

--- deployment/src/utils.ts
+++ deployment/src/utils.ts
@@ -1,8 +1,14 @@
 import { exec, spawn } from 'child_process';
 import path from 'path';
+import fs from 'fs';
 
 export function buildProject(id: string) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+
+        const outputPath = path.join(__dirname, `dist/${id}`);
+        const checkIfBuilt = () => fs.existsSync(outputPath);
+
+        const executBuild = () => {
         const child = exec(`cd ${path.join(__dirname, `output/${id}`)} && npm install && npm run build`);
 
         child.stdout?.on('data', function(data) {
@@ -12,7 +18,22 @@ export function buildProject(id: string) {
             console.log('stderr:' + data);
         });
         child.on('close', function(code) {
-            resolve("");
+            if(checkIfBuilt()) {
+                resolve("Build successful");
+            } else {
+                console.log("index.html not found. Building again...");
+                executBuild();
+            }
         });
+    }
+
+    if(!checkIfBuilt()) {
+        executBuild();
+    } else {
+        console.log("Build already exists");
+        resolve("Build already exists");
+    }
+
+
     });
 }
