9aa1537c62cea15210572e98c4996e7c81530664
Aradhya <aradhya@my.yorku.ca>
2024-03-08 14:51:29 -0500

    1

--- upload/dist/index.js
+++ upload/dist/index.js
@@ -1,50 +1,73 @@
 "use strict";
+//access key id : f694c40b9fd70ad53b6734b17af34c84
+//secret access key : c6003326bc5faad7da3504b155858f226048952aa617383439df35a240a26ef0
+//endpoint for S3: https://4cf4007788e49411ce404afbe2a0e268.r2.cloudflarestorage.com
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildProject = void 0;
-const child_process_1 = require("child_process");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const simple_git_1 = __importDefault(require("simple-git"));
+const utils_1 = require("./utils");
+const body_parser_1 = __importDefault(require("body-parser"));
 const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
-function buildProject(id) {
-    return new Promise((resolve, reject) => {
-        const outputPath = path_1.default.join(__dirname, `output/${id}`);
-        const projectPath = path_1.default.join(__dirname, `output/${id}`);
-        let attempts = 0; // Counter to track the number of build attempts
-        const checkIfBuilt = () => fs_1.default.existsSync(outputPath);
-        const executeBuild = () => {
-            if (attempts >= 3) {
-                // Set a maximum number of attempts
-                console.error("Failed to build after multiple attempts");
-                reject("Build failed");
-                return;
-            }
-            attempts++; // Increment the attempt counter
-            const child = (0, child_process_1.exec)(`cd ${projectPath} && npm install && npm run build`, (error) => {
-                if (error) {
-                    console.error("Build command failed", error);
-                    console.log("Build output not found. Building again...");
-                    executeBuild();
-                }
-            });
-            child.on("close", function (code) {
-                if (checkIfBuilt()) {
-                    resolve("Build successful");
-                }
-                else {
-                    console.log("Build output not found. Attempting to build again...");
-                    executeBuild();
-                }
-            });
-        };
-        if (!checkIfBuilt()) {
-            executeBuild();
-        }
-        else {
-            console.log("Build already exists");
-            resolve("Build already exists");
-        }
+const file_1 = require("./file");
+const aws_1 = require("./aws");
+const redis_1 = require("redis");
+// Using environment variables with default values
+const redisHost = process.env.REDIS_HOST || "localhost"; // Use 'redis' if running with Docker Compose and it's the service name
+const redisPort = process.env.REDIS_PORT
+    ? parseInt(process.env.REDIS_PORT)
+    : 6379;
+const publisher = (0, redis_1.createClient)({
+    socket: {
+        host: redisHost,
+        port: redisPort,
+    },
+});
+publisher.connect();
+const subscriber = (0, redis_1.createClient)({
+    socket: {
+        host: redisHost,
+        port: redisPort,
+    },
+});
+subscriber.connect();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(body_parser_1.default.json());
+app.post("/deploy", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const repoUrl = req.body.repoUrl;
+    const id = (0, utils_1.generate)();
+    yield (0, simple_git_1.default)().clone(repoUrl, path_1.default.join(__dirname, `output/${id}`));
+    const files = (0, file_1.getAllFiles)(path_1.default.join(__dirname, `output/${id}`));
+    files.forEach((file) => __awaiter(void 0, void 0, void 0, function* () {
+        //C:\Users\aradhya\Desktop\Ethan Personal Projects\NexaDeploy\dist\output\zrc3e
+        let normalizedFile = file.replace(/\\/g, "/");
+        yield (0, aws_1.uploadFile)(normalizedFile.slice(__dirname.length + 1), file);
+    }));
+    publisher.lPush("build-queue", id);
+    publisher.hSet("status", id, "uploaded");
+    const value = yield publisher.hGet("status", id);
+    res.json({
+        id: id,
     });
-}
-exports.buildProject = buildProject;
+}));
+app.get("/status", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.query.id;
+    const response = yield subscriber.hGet("status", id);
+    res.json({
+        status: response,
+    });
+}));
+app.listen(3000);
--- upload/src/index.ts
+++ upload/src/index.ts
@@ -1,50 +1,71 @@
-import { exec } from "child_process";
+//access key id : f694c40b9fd70ad53b6734b17af34c84
+//secret access key : c6003326bc5faad7da3504b155858f226048952aa617383439df35a240a26ef0
+//endpoint for S3: https://4cf4007788e49411ce404afbe2a0e268.r2.cloudflarestorage.com
+
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
+import bodyParser from "body-parser";
 import path from "path";
-import fs from "fs";
+import { getAllFiles } from "./file";
+import { uploadFile } from "./aws";
+import { createClient } from "redis";
 
-export function buildProject(id: string) {
-  return new Promise((resolve, reject) => {
-    const outputPath = path.join(__dirname, `output/${id}`);
-    const projectPath = path.join(__dirname, `output/${id}`);
-    let attempts = 0; // Counter to track the number of build attempts
+// Using environment variables with default values
+const redisHost = process.env.REDIS_HOST || "localhost"; // Use 'redis' if running with Docker Compose and it's the service name
+const redisPort = process.env.REDIS_PORT
+  ? parseInt(process.env.REDIS_PORT)
+  : 6379;
 
-    const checkIfBuilt = () => fs.existsSync(outputPath);
+const publisher = createClient({
+  socket: {
+    host: redisHost,
+    port: redisPort,
+  },
+});
+publisher.connect();
 
-    const executeBuild = () => {
-      if (attempts >= 3) {
-        // Set a maximum number of attempts
-        console.error("Failed to build after multiple attempts");
-        reject("Build failed");
-        return;
-      }
-      attempts++; // Increment the attempt counter
+const subscriber = createClient({
+  socket: {
+    host: redisHost,
+    port: redisPort,
+  },
+});
+subscriber.connect();
 
-      const child = exec(
-        `cd ${projectPath} && npm install && npm run build`,
-        (error) => {
-          if (error) {
-            console.error("Build command failed", error);
-            console.log("Build output not found. Building again...");
-            executeBuild();
-          }
-        }
-      );
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-      child.on("close", function (code) {
-        if (checkIfBuilt()) {
-          resolve("Build successful");
-        } else {
-          console.log("Build output not found. Attempting to build again...");
-          executeBuild();
-        }
-      });
-    };
+app.post("/deploy", async (req, res) => {
+  const repoUrl = req.body.repoUrl;
+  const id = generate();
 
-    if (!checkIfBuilt()) {
-      executeBuild();
-    } else {
-      console.log("Build already exists");
-      resolve("Build already exists");
-    }
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+
+  const files = getAllFiles(path.join(__dirname, `output/${id}`));
+
+  files.forEach(async (file) => {
+    //C:\Users\aradhya\Desktop\Ethan Personal Projects\NexaDeploy\dist\output\zrc3e
+
+    let normalizedFile = file.replace(/\\/g, "/");
+    await uploadFile(normalizedFile.slice(__dirname.length + 1), file);
   });
-}
+
+  publisher.lPush("build-queue", id);
+  publisher.hSet("status", id, "uploaded");
+  const value = await publisher.hGet("status", id);
+  res.json({
+    id: id,
+  });
+});
+
+app.get("/status", async (req, res) => {
+  const id = req.query.id;
+  const response = await subscriber.hGet("status", id as string);
+  res.json({
+    status: response,
+  });
+});
+app.listen(3000);
